8b584f8bb1790f7ac729360169c862ed3cd771ab
kaizer1 <nitche9@gmail.com>
2021-03-13 08:32:07 +0300

    Initial Commit

---
+++ TestSceneThree/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestSceneThree
+//
+//  Created by Сергей Лоскутников on 13.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
